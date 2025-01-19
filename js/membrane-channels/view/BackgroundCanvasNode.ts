fb7d14f2def25d0596f350b96398f76cd3785dea
Sam Reid <reids@colorado.edu>
2025-01-18 18:39:26 -0700

    Draw lipids

--- js/membrane-channels/view/BackgroundCanvasNode.ts
+++ js/membrane-channels/view/BackgroundCanvasNode.ts
@@ -9,21 +9,94 @@
  */
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import { CanvasNode } from '../../../../scenery/js/imports.js';
 import MembraneChannelsConstants from '../../common/MembraneChannelsConstants.js';
 import membraneChannels from '../../membraneChannels.js';
 
+// TODO: Move to color file
+const TAIL_COLOR = 'rgb(229,68,143)';
+const LIPID_HEAD_COLOR = 'rgb(248,161,46)';
+const INSIDE_CELL_COLOR = 'rgb(101,185,234)';
+const OUTSIDE_CELL_COLOR = 'rgb(152,205,255)';
+
+// Choose standard units for working on model coordinates. The origin is in the center.
+const modelBounds = new Bounds2( -100, -100, 100, 100 );
+const viewBounds = new Bounds2( 0, 0, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH );
+
 export default class BackgroundCanvasNode extends CanvasNode {
 
+  private readonly modelViewTransform2 = ModelViewTransform2.createRectangleInvertedYMapping( modelBounds, viewBounds );
+  
   public constructor() {
     super( {
-      canvasBounds: new Bounds2( 0, 0, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH )
+      canvasBounds: viewBounds
     } );
   }
 
+  // Convenience functions to move and line in model coordinates
+  private moveTo( context: CanvasRenderingContext2D, x: number, y: number ): void {
+    context.moveTo( this.modelViewTransform2.modelToViewX( x ), this.modelViewTransform2.modelToViewY( y ) );
+  }
+
+  private lineTo( context: CanvasRenderingContext2D, x: number, y: number ): void {
+    context.lineTo( this.modelViewTransform2.modelToViewX( x ), this.modelViewTransform2.modelToViewY( y ) );
+  }
+
   public override paintCanvas( context: CanvasRenderingContext2D ): void {
-    context.fillStyle = 'red';
-    context.fillRect( 0, 0, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH );
+
+    // draw the top half rectangle
+    context.fillStyle = OUTSIDE_CELL_COLOR;
+    context.fillRect( 0, 0, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH / 2 );
+
+    // draw the bottom half rectangle
+    context.fillStyle = INSIDE_CELL_COLOR;
+    context.fillRect( 0, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH / 2, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH, MembraneChannelsConstants.OBSERVATION_WINDOW_WIDTH / 2 );
+
+    const headRadius = 2;
+    const headY = 22;
+
+    // draw the tails first so they appear to be coming out of the heads
+    context.strokeStyle = TAIL_COLOR;
+    context.lineWidth = 2;
+    const tailLength = headY;
+
+    for ( let layer = 0; layer <= 1; layer++ ) {
+      for ( let i = -100; i < 100; i++ ) {
+        context.beginPath();
+        const startY = layer === 0 ? -headY : +headY;
+        const tailDirection = layer === 0 ? 1 : -1;
+
+        this.moveTo( context, i * headRadius * 2, startY );
+        this.lineTo( context, i * headRadius * 2, startY + tailLength * tailDirection );
+        context.stroke();
+      }
+    }
+
+    context.fillStyle = LIPID_HEAD_COLOR;
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+
+    for ( let layer = 0; layer <= 1; layer++ ) {
+      for ( let i = -100; i < 100; i++ ) {
+        // For the phospholipid bilayer, each phospholipid is represented by a circle with a tail for the hydrophobic tail
+        context.beginPath();
+        context.arc( this.modelViewTransform2.modelToViewX( i * headRadius * 2 ), this.modelViewTransform2.modelToViewY( layer === 0 ? -headY : +headY ), this.modelViewTransform2.modelToViewDeltaX( headRadius ), 0, 2 * Math.PI );
+        context.fill();
+        context.stroke();
+      }
+    }
+
+    // draw a cross hairs at the origin, so we can see where it is
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+    context.beginPath();
+    this.moveTo( context, 0, -5 );
+    this.lineTo( context, 0, 5 );
+    this.moveTo( context, -5, 0 );
+    this.lineTo( context, 5, 0 );
+
+    context.stroke();
   }
 }
 membraneChannels.register( 'BackgroundCanvasNode', BackgroundCanvasNode );
