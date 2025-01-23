aeae964545c2248d95bfad150900d23188f52985
Sam Reid <reids@colorado.edu>
2025-01-23 06:13:24 -0700

    Add TODO

--- js/membrane-channels/view/MembraneChannelsAccordionBoxGroup.ts
+++ js/membrane-channels/view/MembraneChannelsAccordionBoxGroup.ts
@@ -10,6 +10,8 @@ import membraneChannels from '../../membraneChannels.js';
 /**
  * Shows the title and group of accordion boxes for the membrane channels, which can be dragged into the play area.
  *
+ * TODO: Better name to indicate this is for a control panel and not named MembraneChannels because of the sim name?
+ *
  * TODO:
  * - maxWidth
  * - dynamic layout
