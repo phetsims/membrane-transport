fc16ee292ba6c30e756e3ffa96d89aa4caeab78a
Sam Reid <reids@colorado.edu>
2025-01-24 07:11:19 -0700

    Add solute nodes

--- js/membrane-channels/view/solutes/GlucoseNode.ts
+++ js/membrane-channels/view/solutes/GlucoseNode.ts
@@ -48,6 +48,7 @@ export default class GlucoseNode extends Node {
 
       .close();
 
+    // TODO: Should we squash the path vertically then add an unsquashed stroke to get the ellipse gradient?
     const gradient = new RadialGradient( 0, 0, 2, 0, 0, radiusX )
       .addColorStop( 0, membraneChannelsColors.glucoseBarChartColorProperty.value.colorUtilsDarker( 0.3 ).toCSS() )
       .addColorStop( 0.7, membraneChannelsColors.glucoseBarChartColorProperty.value.colorUtilsBrighter( 0.8 ).toCSS() );
