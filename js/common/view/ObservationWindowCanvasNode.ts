dd99aa148da44979976a8ddb1663efc7680f3d1d
Sam Reid <reids@colorado.edu>
2025-02-05 18:50:31 -0700

    Factor out Phospholipid.ts

--- js/common/view/ObservationWindowCanvasNode.ts
+++ js/common/view/ObservationWindowCanvasNode.ts
@@ -9,7 +9,6 @@
  */
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
-import dotRandom from '../../../../dot/js/dotRandom.js';
 import Utils from '../../../../dot/js/Utils.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
@@ -20,58 +19,30 @@ import membraneChannels from '../../membraneChannels.js';
 import { getFeatureSetSoluteTypes } from '../MembraneChannelsFeatureSet.js';
 import MembraneChannelsModel from '../model/MembraneChannelsModel.js';
 import SoluteType from '../model/SoluteType.js';
+import Phospholipid from './Phospholipid.js';
 import getSoluteNode from './solutes/getSoluteNode.js';
-
-// Head parameters
-const headRadius = 1.3;
-
-// Constants controlling the tail control point movement
-const controlPointStepSize = 0.1; // the random component for the change in velocity
-const friction = 0.9999;          // a friction coefficient for momentum (0 to 1)
-
-// Introduce a positive-valued "angle" (really a horizontal offset) to separate the two tails
-// from each lipid head. Adjust as needed for appearance.
-const TAIL_OFFSET = 0.3;
-
-// Define an interface for a control point.
-// We add a velocity component "vx" for horizontal momentum.
-type ControlPoint = {
-  x: number;
-  y: number;
-  vx: number;
-};
-
-// For each tail, we want to store its anchor (head center) and a list of control points.
-type TailState = {
-  anchorX: number;
-  anchorY: number;
-  controlPoints: ControlPoint[];
-};
 
 export default class ObservationWindowCanvasNode extends CanvasNode {
 
-  private time = 0;
-
-  // We use separate tail states for inner and outer layers, each with two tails per head.
-  private tailStatesInner: TailState[] = [];
-  private tailStatesOuter: TailState[] = [];
-
-  private readonly headY: number;
-
   private readonly soluteTypeToImageMap = new Map<SoluteType, HTMLImageElement | HTMLCanvasElement>();
+  private readonly phospholipids: Phospholipid[] = [];
 
   public constructor( private readonly model: MembraneChannelsModel, private readonly modelViewTransform: ModelViewTransform2, canvasBounds: Bounds2 ) {
     super( {
       canvasBounds: canvasBounds
     } );
 
-    // So that the edge of the head is at the edge of the bounds.
-    this.headY = MembraneChannelsConstants.MEMBRANE_BOUNDS.maxY - headRadius;
-
     getFeatureSetSoluteTypes( model.featureSet ).forEach( soluteType => {
       this.soluteTypeToImageMap.set( soluteType, this.createImage( soluteType ) );
     } );
-    this.initializeTailStates();
+
+    // TODO: Make sure not too many tails, you can see they go out of bounds if you remove the clip area
+    for ( let i = -40; i <= 40; i++ ) {
+
+      const anchorX = i * Phospholipid.headRadius * 2;
+      this.phospholipids.push( new Phospholipid( 'inner', anchorX, modelViewTransform ) );
+      this.phospholipids.push( new Phospholipid( 'outer', anchorX, modelViewTransform ) );
+    }
   }
 
   private createImage( soluteType: SoluteType ): HTMLImageElement | HTMLCanvasElement {
@@ -93,126 +64,13 @@ export default class ObservationWindowCanvasNode extends CanvasNode {
   }
 
   public step( dt: number ): void {
-    this.time = this.time + dt;
 
-    this.updateTailStatesFor( dt, this.tailStatesInner );
-    this.updateTailStatesFor( dt, this.tailStatesOuter );
-  }
+    if ( this.model.isPlayingProperty.value ) {
 
-  // Initialize two sets of tail states (inner and outer). For each "head" position,
-  // we now create two tails instead of one by horizontally offsetting each tail’s anchor.
-  private initializeTailStates(): void {
-
-    // TODO: Make sure not too many tails, you can see they go out of bounds if you remove the clip area
-    for ( let i = -40; i <= 40; i++ ) {
-
-      // For each head, its center is anchorX on the horizontal axis
-      const anchorX = i * headRadius * 2;
-
-      // We'll create two separate anchorX positions, each offset by ± TAIL_ANGLE/2
-      // so that we have two tails at slightly different angles.
-      const anchorXLeft = anchorX - TAIL_OFFSET / 2;
-      const anchorXRight = anchorX + TAIL_OFFSET / 2;
-
-      // 1) Inner side: anchorY is -headY
-      const innerAnchorY = -this.headY;
-
-      // Tail A (left offset) for the inner side
-      const cp1InnerLeft: ControlPoint = { x: anchorXLeft, y: innerAnchorY + this.headY / 2, vx: 0 };
-      const cp2InnerLeft: ControlPoint = { x: anchorXLeft, y: innerAnchorY + this.headY, vx: 0 };
-      this.tailStatesInner.push( {
-        anchorX: anchorXLeft,
-        anchorY: innerAnchorY,
-        controlPoints: [ cp1InnerLeft, cp2InnerLeft ]
-      } );
-
-      // Tail B (right offset) for the inner side
-      const cp1InnerRight: ControlPoint = { x: anchorXRight, y: innerAnchorY + this.headY / 2, vx: 0 };
-      const cp2InnerRight: ControlPoint = { x: anchorXRight, y: innerAnchorY + this.headY, vx: 0 };
-      this.tailStatesInner.push( {
-        anchorX: anchorXRight,
-        anchorY: innerAnchorY,
-        controlPoints: [ cp1InnerRight, cp2InnerRight ]
-      } );
-
-      // 2) Outer side: anchorY is +headY
-      const outerAnchorY = this.headY;
-
-      // Tail A (left offset) for the outer side
-      const cp1OuterLeft: ControlPoint = { x: anchorXLeft, y: outerAnchorY - this.headY / 2, vx: 0 };
-      const cp2OuterLeft: ControlPoint = { x: anchorXLeft, y: outerAnchorY - this.headY, vx: 0 };
-      this.tailStatesOuter.push( {
-        anchorX: anchorXLeft,
-        anchorY: outerAnchorY,
-        controlPoints: [ cp1OuterLeft, cp2OuterLeft ]
-      } );
-
-      // Tail B (right offset) for the outer side
-      const cp1OuterRight: ControlPoint = { x: anchorXRight, y: outerAnchorY - this.headY / 2, vx: 0 };
-      const cp2OuterRight: ControlPoint = { x: anchorXRight, y: outerAnchorY - this.headY, vx: 0 };
-      this.tailStatesOuter.push( {
-        anchorX: anchorXRight,
-        anchorY: outerAnchorY,
-        controlPoints: [ cp1OuterRight, cp2OuterRight ]
-      } );
-    }
-  }
-
-  // Update control point positions via a random walk with momentum, for a given set of tail states.
-  private updateTailStatesFor( dt: number, tailStates: TailState[] ): void {
-    for ( const state of tailStates ) {
-      // Define horizontal bounds relative to the tail's anchor.
-      const tailWindowSize = 1;
-      const minX = state.anchorX - tailWindowSize;
-      const maxX = state.anchorX + tailWindowSize;
-      // For each control point, update its x using momentum, leaving y unchanged.
-      for ( const cp of state.controlPoints ) {
-        cp.vx = cp.vx * friction + ( dotRandom.nextDouble() * 2 - 1 ) * controlPointStepSize * dt;
-        cp.x += cp.vx;
-        // Clamp the x position to within [anchorX - tailWindowSize, anchorX + tailWindowSize]
-        if ( cp.x < minX ) {
-          cp.x = minX;
-          cp.vx = 0;
-        }
-        else if ( cp.x > maxX ) {
-          cp.x = maxX;
-          cp.vx = 0;
-        }
+      // Update the phospholipids
+      for ( let i = 0; i < this.phospholipids.length; i++ ) {
+        this.phospholipids[ i ].step( dt );
       }
-    }
-  }
-
-  /**
-   * Draw tails for the given side ("inner" or "outer").
-   */
-  public drawTails( context: CanvasRenderingContext2D, side: 'inner' | 'outer' ): void {
-
-    context.strokeStyle = MembraneChannelsColors.lipidTailColorProperty.value.toCSS();
-    context.lineWidth = 2;
-
-    // For each tail state, draw a cubic Bézier curve using the anchor, the two control points,
-    // and then a tail endpoint defined relative to the last control point.
-    // (Adjust the endpoint offset if needed.)
-    const OFFSET = 0.8;
-    const endpointOffset = side === 'inner' ? -OFFSET : OFFSET;
-
-    const tailStates = side === 'inner' ? this.tailStatesInner : this.tailStatesOuter;
-
-    for ( const state of tailStates ) {
-      context.beginPath();
-
-      // The last control point helps define our endpoint
-      const lastCP = state.controlPoints[ state.controlPoints.length - 1 ];
-      const tailEndX = lastCP.x;
-      const tailEndY = lastCP.y + endpointOffset;
-
-      this.moveTo( context, state.anchorX, state.anchorY );
-      context.bezierCurveTo(
-        this.modelViewTransform.modelToViewX( state.controlPoints[ 0 ].x ), this.modelViewTransform.modelToViewY( state.controlPoints[ 0 ].y ),
-        this.modelViewTransform.modelToViewX( state.controlPoints[ 1 ].x ), this.modelViewTransform.modelToViewY( state.controlPoints[ 1 ].y ),
-        this.modelViewTransform.modelToViewX( tailEndX ), this.modelViewTransform.modelToViewY( tailEndY )
-      );
-      context.stroke();
     }
   }
 
@@ -302,41 +160,14 @@ export default class ObservationWindowCanvasNode extends CanvasNode {
 
     this.drawSolutes( context );
 
-    // Draw tails independently for inner and outer layers (which now each have 2 tails per head).
-    this.drawTails( context, 'inner' );
-    this.drawTails( context, 'outer' );
-
-    // --- Draw the heads ---
-    context.fillStyle = MembraneChannelsColors.lipidHeadColorProperty.value.toCSS();
-    context.strokeStyle = 'black';
-    context.lineWidth = 2;
-
-    // Draw inner heads
-    // TODO: Make sure not too many heads, you can see they go out of bounds if you remove the clip area
-    for ( let i = -100; i < 100; i++ ) {
-      context.beginPath();
-      context.arc(
-        this.modelViewTransform.modelToViewX( i * headRadius * 2 ),
-        this.modelViewTransform.modelToViewY( -this.headY ), // inner heads
-        this.modelViewTransform.modelToViewDeltaX( headRadius ),
-        0, 2 * Math.PI
-      );
-      context.fill();
-      context.stroke();
+    Phospholipid.initTails( context );
+    for ( let i = 0; i < this.phospholipids.length; i++ ) {
+      this.phospholipids[ i ].drawTails( context );
     }
 
-    // Draw outer heads
-    // TODO: Make sure not too many heads, you can see they go out of bounds if you remove the clip area
-    for ( let i = -100; i < 100; i++ ) {
-      context.beginPath();
-      context.arc(
-        this.modelViewTransform.modelToViewX( i * headRadius * 2 ),
-        this.modelViewTransform.modelToViewY( this.headY ),  // outer heads
-        this.modelViewTransform.modelToViewDeltaX( headRadius ),
-        0, 2 * Math.PI
-      );
-      context.fill();
-      context.stroke();
+    Phospholipid.initHeads( context );
+    for ( let i = 0; i < this.phospholipids.length; i++ ) {
+      this.phospholipids[ i ].drawHead( context );
     }
 
     // --- Debugging code to check transforms and bounds ---
---
+++ js/common/view/Phospholipid.ts
@@ -0,0 +1,208 @@
+// Copyright 2025, University of Colorado Boulder
+
+/**
+ * Contains model computation and view rendering for the phospholipids, which are transient and not part of PhET-iO state.
+ *
+ * NOTE: It is rendered in canvas, so it does not extend Node.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
+import dotRandom from '../../../../dot/js/dotRandom.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import membraneChannels from '../../membraneChannels.js';
+import MembraneChannelsColors from '../MembraneChannelsColors.js';
+import MembraneChannelsConstants from '../MembraneChannelsConstants.js';
+
+// Constants controlling the tail control point movement
+const controlPointStepSize = 0.1; // the random component for the change in velocity
+const friction = 0.9999;          // a friction coefficient for momentum (0 to 1)
+
+// Introduce a positive-valued "angle" (really a horizontal offset) to separate the two tails
+// from each lipid head. Adjust as needed for appearance.
+const TAIL_OFFSET = 0.3;
+
+// Define an interface for a control point.
+// We add a velocity component "vx" for horizontal momentum.
+type ControlPoint = {
+  x: number;
+  y: number;
+  vx: number;
+};
+
+// For each tail, we want to store its anchor (head center) and a list of control points.
+type TailState = {
+  anchorX: number;
+  anchorY: number;
+  controlPoints: ControlPoint[];
+};
+
+export default class Phospholipid {
+  private readonly headY: number;
+
+  private readonly tailStates: TailState[] = [];
+
+  // Head parameters
+  public static readonly headRadius = 1.3;
+
+  public constructor(
+    public readonly side: 'outer' | 'inner',
+    public readonly anchorX: number,
+    private readonly modelViewTransform: ModelViewTransform2
+  ) {
+    // So that the edge of the head is at the edge of the bounds.
+    this.headY = MembraneChannelsConstants.MEMBRANE_BOUNDS.maxY - Phospholipid.headRadius;
+
+    // For each head, its center is anchorX on the horizontal axis
+
+    // We'll create two separate anchorX positions, each offset by ± TAIL_ANGLE/2
+    // so that we have two tails at slightly different angles.
+    const anchorXLeft = anchorX - TAIL_OFFSET / 2;
+    const anchorXRight = anchorX + TAIL_OFFSET / 2;
+
+    if ( this.side === 'inner' ) {
+
+      // 1) Inner side: anchorY is -headY
+      const innerAnchorY = -this.headY;
+
+      // Tail A (left offset) for the inner side
+      const cp1InnerLeft: ControlPoint = { x: anchorXLeft, y: innerAnchorY + this.headY / 2, vx: 0 };
+      const cp2InnerLeft: ControlPoint = { x: anchorXLeft, y: innerAnchorY + this.headY, vx: 0 };
+      this.tailStates.push( {
+        anchorX: anchorXLeft,
+        anchorY: innerAnchorY,
+        controlPoints: [ cp1InnerLeft, cp2InnerLeft ]
+      } );
+
+      // Tail B (right offset) for the inner side
+      const cp1InnerRight: ControlPoint = { x: anchorXRight, y: innerAnchorY + this.headY / 2, vx: 0 };
+      const cp2InnerRight: ControlPoint = { x: anchorXRight, y: innerAnchorY + this.headY, vx: 0 };
+      this.tailStates.push( {
+        anchorX: anchorXRight,
+        anchorY: innerAnchorY,
+        controlPoints: [ cp1InnerRight, cp2InnerRight ]
+      } );
+    }
+
+    if ( this.side === 'outer' ) {
+      // 2) Outer side: anchorY is +headY
+      const outerAnchorY = this.headY;
+
+      // Tail A (left offset) for the outer side
+      const cp1OuterLeft: ControlPoint = { x: anchorXLeft, y: outerAnchorY - this.headY / 2, vx: 0 };
+      const cp2OuterLeft: ControlPoint = { x: anchorXLeft, y: outerAnchorY - this.headY, vx: 0 };
+      this.tailStates.push( {
+        anchorX: anchorXLeft,
+        anchorY: outerAnchorY,
+        controlPoints: [ cp1OuterLeft, cp2OuterLeft ]
+      } );
+
+      // Tail B (right offset) for the outer side
+      const cp1OuterRight: ControlPoint = { x: anchorXRight, y: outerAnchorY - this.headY / 2, vx: 0 };
+      const cp2OuterRight: ControlPoint = { x: anchorXRight, y: outerAnchorY - this.headY, vx: 0 };
+      this.tailStates.push( {
+        anchorX: anchorXRight,
+        anchorY: outerAnchorY,
+        controlPoints: [ cp1OuterRight, cp2OuterRight ]
+      } );
+    }
+  }
+
+  public static initHeads( context: CanvasRenderingContext2D ): void {
+    context.fillStyle = MembraneChannelsColors.lipidHeadColorProperty.value.toCSS();
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+  }
+
+  public drawHead( context: CanvasRenderingContext2D ): void {
+
+    // --- Draw the heads ---
+
+    // Draw inner heads
+    // TODO: Make sure not too many heads, you can see they go out of bounds if you remove the clip area
+    context.beginPath();
+    context.arc(
+      this.modelViewTransform.modelToViewX( this.anchorX ),
+      this.modelViewTransform.modelToViewY( -this.headY ), // inner heads
+      this.modelViewTransform.modelToViewDeltaX( Phospholipid.headRadius ),
+      0, 2 * Math.PI
+    );
+    context.fill();
+    context.stroke();
+
+    // Draw outer heads
+    // TODO: Make sure not too many heads, you can see they go out of bounds if you remove the clip area
+    context.beginPath();
+    context.arc(
+      this.modelViewTransform.modelToViewX( this.anchorX ),
+      this.modelViewTransform.modelToViewY( this.headY ),  // outer heads
+      this.modelViewTransform.modelToViewDeltaX( Phospholipid.headRadius ),
+      0, 2 * Math.PI
+    );
+    context.fill();
+    context.stroke();
+  }
+
+  public static initTails( context: CanvasRenderingContext2D ): void {
+    context.strokeStyle = MembraneChannelsColors.lipidTailColorProperty.value.toCSS();
+    context.lineWidth = 2;
+  }
+
+  public drawTails( context: CanvasRenderingContext2D ): void {
+
+    // For each tail state, draw a cubic Bézier curve using the anchor, the two control points,
+    // and then a tail endpoint defined relative to the last control point.
+    // (Adjust the endpoint offset if needed.)
+    const OFFSET = 0.8;
+    const endpointOffset = this.side === 'inner' ? -OFFSET : OFFSET;
+
+    for ( const state of this.tailStates ) {
+      context.beginPath();
+
+      // The last control point helps define our endpoint
+      const lastCP = state.controlPoints[ state.controlPoints.length - 1 ];
+      const tailEndX = lastCP.x;
+      const tailEndY = lastCP.y + endpointOffset;
+
+      this.moveTo( context, state.anchorX, state.anchorY );
+      context.bezierCurveTo(
+        this.modelViewTransform.modelToViewX( state.controlPoints[ 0 ].x ), this.modelViewTransform.modelToViewY( state.controlPoints[ 0 ].y ),
+        this.modelViewTransform.modelToViewX( state.controlPoints[ 1 ].x ), this.modelViewTransform.modelToViewY( state.controlPoints[ 1 ].y ),
+        this.modelViewTransform.modelToViewX( tailEndX ), this.modelViewTransform.modelToViewY( tailEndY )
+      );
+      context.stroke();
+    }
+  }
+
+  // Convenience functions to move and line in model coordinates
+  private moveTo( context: CanvasRenderingContext2D, x: number, y: number ): void {
+    context.moveTo( this.modelViewTransform.modelToViewX( x ), this.modelViewTransform.modelToViewY( y ) );
+  }
+
+  // Update control point positions via a random walk with momentum, for a given set of tail states.
+  public step( dt: number ): void {
+    for ( const state of this.tailStates ) {
+      // Define horizontal bounds relative to the tail's anchor.
+      const tailWindowSize = 1;
+      const minX = state.anchorX - tailWindowSize;
+      const maxX = state.anchorX + tailWindowSize;
+      // For each control point, update its x using momentum, leaving y unchanged.
+      for ( const cp of state.controlPoints ) {
+        cp.vx = cp.vx * friction + ( dotRandom.nextDouble() * 2 - 1 ) * controlPointStepSize * dt;
+        cp.x += cp.vx;
+        // Clamp the x position to within [anchorX - tailWindowSize, anchorX + tailWindowSize]
+        if ( cp.x < minX ) {
+          cp.x = minX;
+          cp.vx = 0;
+        }
+        else if ( cp.x > maxX ) {
+          cp.x = maxX;
+          cp.vx = 0;
+        }
+      }
+    }
+  }
+}
+
+membraneChannels.register( 'Phospholipid', Phospholipid );
