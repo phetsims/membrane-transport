abf8f3ec74ad5321353ed180c7a418ea484bc73b
Sam Reid <reids@colorado.edu>
2025-04-07 08:46:13 -0600

    Rename glucoseSodium => sodiumGlucose

--- js/common/view/proteins/SodiumGlucoseCotransporterNode.ts
+++ js/common/view/proteins/SodiumGlucoseCotransporterNode.ts
@@ -8,10 +8,9 @@
 
 import Image from '../../../../../scenery/js/nodes/Image.js';
 
-// TODO: @Brett At a meeting, we decided to call it "sodiumGlucoseCotransporter" but the artwork came in as "glucoseSodiumCotransporter", which should we go with?
-import glucoseSodiumCotransporterState1_svg from '../../../../images/glucoseSodiumCotransporterState1_svg.js';
-import glucoseSodiumCotransporterState2_svg from '../../../../images/glucoseSodiumCotransporterState2_svg.js';
-import glucoseSodiumCotransporterState3_svg from '../../../../images/glucoseSodiumCotransporterState3_svg.js';
+import sodiumGlucoseCotransporterState1_svg from '../../../../images/sodiumGlucoseCotransporterState1_svg.js';
+import sodiumGlucoseCotransporterState2_svg from '../../../../images/sodiumGlucoseCotransporterState2_svg.js';
+import sodiumGlucoseCotransporterState3_svg from '../../../../images/sodiumGlucoseCotransporterState3_svg.js';
 import membraneTransport from '../../../membraneTransport.js';
 import MembraneTransportConstants from '../../MembraneTransportConstants.js';
 import SodiumGlucoseCotransporter from '../../model/proteins/SodiumGlucoseCotransporter.js';
@@ -25,7 +24,7 @@ export default class SodiumGlucoseCotransporterNode extends ProteinNode {
   public constructor( public readonly sodiumGlucoseCotransporter: SodiumGlucoseCotransporter | null ) {
 
     super();
-    this.image = new Image( glucoseSodiumCotransporterState1_svg );
+    this.image = new Image( sodiumGlucoseCotransporterState1_svg );
     this.image.setScaleMagnitude( MembraneTransportConstants.TRANSPORT_PROTEIN_IMAGE_SCALE );
 
     this.addChild( this.image );
@@ -34,9 +33,9 @@ export default class SodiumGlucoseCotransporterNode extends ProteinNode {
   public override step( dt: number ): void {
     if ( this.sodiumGlucoseCotransporter ) {
       console.log( this.sodiumGlucoseCotransporter.conformation );
-      this.image.image = this.sodiumGlucoseCotransporter.conformation === 'awaiting-sodium' ? glucoseSodiumCotransporterState1_svg :
-                         this.sodiumGlucoseCotransporter.conformation === 'awaiting-glucose' ? glucoseSodiumCotransporterState2_svg :
-                         glucoseSodiumCotransporterState3_svg;
+      this.image.image = this.sodiumGlucoseCotransporter.conformation === 'awaiting-sodium' ? sodiumGlucoseCotransporterState1_svg :
+                         this.sodiumGlucoseCotransporter.conformation === 'awaiting-glucose' ? sodiumGlucoseCotransporterState2_svg :
+                         sodiumGlucoseCotransporterState3_svg;
     }
   }
 }
