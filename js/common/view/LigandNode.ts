5f1b0f1a8494a46b855b8cae03f320ebc12449d2
Sam Reid <reids@colorado.edu>
2025-04-17 09:23:54 -0600

    Update TODOs

--- js/common/view/LigandNode.ts
+++ js/common/view/LigandNode.ts
@@ -7,6 +7,7 @@
  */
 
 // TODO: The focus rectangle shows on the membrane when a ligand has focus. This is incorrect.
+// TODO (design): Rewrite keyboard interaction to be discrete instead of continuous
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import TProperty from '../../../../axon/js/TProperty.js';
