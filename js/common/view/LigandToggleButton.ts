e14cbe70f437378c10367637fe0299b6ce9bfa53
Sam Reid <reids@colorado.edu>
2025-04-22 10:29:10 -0600

    Update TODO, see https://github.com/phetsims/membrane-transport/issues/106

--- js/common/view/LigandToggleButton.ts
+++ js/common/view/LigandToggleButton.ts
@@ -52,7 +52,7 @@ export default class LigandToggleButton extends BooleanRectangularToggleButton {
     } );
 
     // TODO (JG): High level API for context responses? See https://github.com/phetsims/membrane-transport/issues/10
-    // TODO (SR): What if you set areLigandsAddedProperty programmatically? Should it speak when changed with phet-io? - lets discuss with design. https://github.com/phetsims/membrane-transport/issues/86
+    // TODO (SR): What if you set areLigandsAddedProperty programmatically? Should it speak when changed with phet-io? - lets discuss with design. https://github.com/phetsims/membrane-transport/issues/106
     //   We could have a shut-off valve for all responses when something is set by PhET-iO.
     //   OR The high level API could speak the response from user input (like sound implementation, see produceSoundEmitter)
     //   Consensus: We should not speak when the model changes, only when the user interacts with the UI. The screen summary "current details" will mention whether ligands are here or not.
