b48e73fe65771b45459790577eb2615f40b52bf8
Sam Reid <reids@colorado.edu>
2025-04-24 09:20:49 -0600

    Add discrete keyboard interaction for ligands, see https://github.com/phetsims/membrane-transport/issues/45

--- js/common/view/ObservationWindow.ts
+++ js/common/view/ObservationWindow.ts
@@ -133,7 +133,9 @@ export default class ObservationWindow extends InteractiveHighlightingNode {
           isFocusable,
 
           // TODO (phet-io/design) instrument all LigandNodes, since they can all be dragged with mouse? See https://github.com/phetsims/membrane-transport/issues/32
-          isFocusable ? tandem.createTandem( ligand.type === 'ligandA' ? 'ligandANode' : 'ligandBNode' ) : Tandem.OPT_OUT
+          isFocusable ? tandem.createTandem( ligand.type === 'ligandA' ? 'ligandANode' : 'ligandBNode' ) : Tandem.OPT_OUT,
+
+          this
         );
         this.ligandNodes.push( ligandNode );
       } );
