c00d20a9a0233d0e760d8edbd6ec10a15b992485
Sam Reid <reids@colorado.edu>
2025-05-19 23:29:17 -0600

    Connect lines from the radio buttons to their corresponding positions on the arrow, see https://github.com/phetsims/membrane-transport/issues/173

--- js/common/view/MembranePotentialPanel.ts
+++ js/common/view/MembranePotentialPanel.ts
@@ -7,7 +7,13 @@
  */
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import { linear } from '../../../../dot/js/util/linear.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
+import ManualConstraint from '../../../../scenery/js/layout/constraints/ManualConstraint.js';
 import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
+import Line, { LineOptions } from '../../../../scenery/js/nodes/Line.js';
+import Node from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import RectangularRadioButtonGroup from '../../../../sun/js/buttons/RectangularRadioButtonGroup.js';
 import Checkbox from '../../../../sun/js/Checkbox.js';
@@ -47,10 +53,19 @@ export default class MembranePotentialPanel extends Panel {
       orientation: 'horizontal',
       accessibleName: MembraneTransportStrings.a11y.transportProteinPanel.voltageGatedChannelPanel.membranePotential.radioButtonGroup.accessibleNameStringProperty,
       accessibleHelpText: MembraneTransportStrings.a11y.transportProteinPanel.voltageGatedChannelPanel.membranePotential.radioButtonGroup.accessibleHelpTextStringProperty,
-      tandem: tandem.createTandem( 'membranePotentialRadioButtonGroup' )
+      tandem: tandem.createTandem( 'membranePotentialRadioButtonGroup' ),
+      spacing: 35
     } );
 
-    const content = new VBox( {
+
+    const arrowNode = new ArrowNode( 0, 0, 160, 0, {
+      fill: 'black',
+      stroke: null,
+      tailWidth: 2,
+      doubleHead: true
+    } );
+
+    const vbox = new VBox( {
       spacing: 4,
       align: 'left',
       children: [
@@ -59,6 +74,8 @@ export default class MembranePotentialPanel extends Panel {
           maxWidth: 160,
           fontSize: 14
         } ),
+
+        arrowNode,
 
         membranePotentialRadioButtonGroup,
 
@@ -77,11 +94,68 @@ export default class MembranePotentialPanel extends Panel {
         } )
       ]
     } );
+
+    // Connect lines from the radio buttons to their corresponding positions on the arrow
+    const lineOptions: LineOptions = {
+      stroke: 'black',
+      lineWidth: 1
+    };
+
+    const lineNegative70 = new Line( 0, 0, 0, 0, lineOptions );
+    const tickNegative70 = new Line( 0, 0, 0, 0, lineOptions );
+    const lineNegative50 = new Line( 0, 0, 0, 0, lineOptions );
+    const tickNegative50 = new Line( 0, 0, 0, 0, lineOptions );
+    const linePositive30 = new Line( 0, 0, 0, 0, lineOptions );
+    const tickPositive30 = new Line( 0, 0, 0, 0, lineOptions );
+
+    const content = new Node( {
+      children: [
+        lineNegative70,
+        lineNegative50,
+        linePositive30,
+        tickNegative70,
+        tickNegative50,
+        tickPositive30,
+        vbox
+      ]
+    } );
+
     super( content, {
       cornerRadius: 4,
       stroke: null,
       fill: 'rgb(230,229,229)',
       tandem: tandem
+    } );
+
+    const button1 = membranePotentialRadioButtonGroup.getButtonForValue( -70 );
+    const button2 = membranePotentialRadioButtonGroup.getButtonForValue( -50 );
+    const button3 = membranePotentialRadioButtonGroup.getButtonForValue( 30 );
+    ManualConstraint.create( this, [ arrowNode, button1, button2, button3, lineNegative70 ], ( arrowNodeProxy, button1Proxy, line1Proxy ) => {
+
+      const INSET = 30;
+
+      // linear map to find positions along the arrow.
+      const a = linear( -70, 30, arrowNode.globalBounds.left + INSET, arrowNode.globalBounds.right - INSET, -70 );
+      const b = linear( -70, 30, arrowNode.globalBounds.left + INSET, arrowNode.globalBounds.right - INSET, -50 );
+      const c = linear( -70, 30, arrowNode.globalBounds.left + INSET, arrowNode.globalBounds.right - INSET, 30 );
+
+      lineNegative70.setPoint1( lineNegative70.globalToParentPoint( button1.globalBounds.centerTop ) );
+      lineNegative70.setPoint2( lineNegative70.globalToParentPoint( new Vector2( a, arrowNode.globalBounds.centerY ) ) );
+
+      lineNegative50.setPoint1( lineNegative50.globalToParentPoint( button2.globalBounds.centerTop ) );
+      lineNegative50.setPoint2( lineNegative50.globalToParentPoint( new Vector2( b, arrowNode.globalBounds.centerY ) ) );
+
+      linePositive30.setPoint1( linePositive30.globalToParentPoint( button3.globalBounds.centerTop ) );
+      linePositive30.setPoint2( linePositive30.globalToParentPoint( new Vector2( c, arrowNode.globalBounds.centerY ) ) );
+
+      tickNegative70.setPoint1( lineNegative70.p2 );
+      tickNegative70.setPoint2( lineNegative70.p2.plusXY( 0, -5 ) );
+
+      tickNegative50.setPoint1( lineNegative50.p2 );
+      tickNegative50.setPoint2( lineNegative50.p2.plusXY( 0, -5 ) );
+
+      tickPositive30.setPoint1( linePositive30.p2 );
+      tickPositive30.setPoint2( linePositive30.p2.plusXY( 0, -5 ) );
     } );
   }
 }
