50b3aa5e8ac4ce619c2fdcae52d561bff4b30c2c
phet-dev <phetadmin@gmail.com>
2025-06-14 03:48:22 -0600

    grunt update  from daily grunt work

--- js/MembraneTransportFluent.ts
+++ js/MembraneTransportFluent.ts
@@ -6,10 +6,10 @@
 /* @formatter:off */
 
 import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
-import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
-import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
-import type {FluentVariable} from '../../chipper/js/browser/FluentPattern.js';
+import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
 import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
+import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
+import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
 import membraneTransport from './membraneTransport.js';
 import MembraneTransportStrings from './MembraneTransportStrings.js';
 
