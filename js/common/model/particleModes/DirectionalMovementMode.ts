840d96ac098f516827efcb1b7a3718d2686d2b29
Sam Reid <reids@colorado.edu>
2025-06-17 11:38:29 -0600

    Factor out duplicated code between PassiveDiffusionMode and MovingThroughTransportProteinMode, see https://github.com/phetsims/membrane-transport/issues/236

---
+++ js/common/model/particleModes/DirectionalMovementMode.ts
@@ -0,0 +1,75 @@
+// Copyright 2025, University of Colorado Boulder
+
+/**
+ * Abstract base class for particle modes that involve directional movement across the membrane.
+ * Factors out common movement logic shared between PassiveDiffusionMode and MovingThroughTransportProteinMode.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
+import dotRandom from '../../../../../dot/js/dotRandom.js';
+import { boxMullerTransform } from '../../../../../dot/js/util/boxMullerTransform.js';
+import { clamp } from '../../../../../dot/js/util/clamp.js';
+import Vector2 from '../../../../../dot/js/Vector2.js';
+import IntentionalAny from '../../../../../phet-core/js/types/IntentionalAny.js';
+import membraneTransport from '../../../membraneTransport.js';
+import MembraneTransportConstants from '../../MembraneTransportConstants.js';
+import MembraneTransportModel from '../MembraneTransportModel.js';
+import Particle from '../Particle.js';
+import BaseParticleMode from './BaseParticleMode.js';
+import { ParticleModeType } from './BaseParticleMode.js';
+
+export default abstract class DirectionalMovementMode extends BaseParticleMode {
+
+  protected constructor( type: ParticleModeType, public readonly direction: 'inward' | 'outward' ) {
+    super( type );
+  }
+
+  public step( dt: number, particle: Particle<IntentionalAny>, model: MembraneTransportModel ): void {
+    this.performDirectionalMovement( dt, particle, model );
+    this.handleSpecificBehavior( dt, particle, model );
+    this.handleBoundaryBehavior( dt, particle );
+  }
+
+  protected performDirectionalMovement( dt: number, particle: Particle<IntentionalAny>, model: MembraneTransportModel ): void {
+    const sign = this.direction === 'inward' ? -1 : 1;
+    const signBefore = particle.position.y > 0;
+    const TYPICAL_SPEED = 30;
+
+    particle.position.y += sign * ( TYPICAL_SPEED / 5 ) * dt * dotRandom.nextDoubleBetween( 0.1, 2 );
+    particle.position.x += dotRandom.nextDoubleBetween( -2, 2 ) * ( TYPICAL_SPEED / 2 ) * dt;
+
+    const signAfter = particle.position.y > 0;
+
+    if ( signBefore !== signAfter ) {
+      model.soluteCrossedMembraneEmitter.emit( particle, particle.position.y > 0 ? 'outward' : 'inward' );
+    }
+  }
+
+  protected sampleValueHowLongToGoStraight(): number {
+    const result = boxMullerTransform( 0.3, 0.4, dotRandom );
+    return clamp( result, 0.01, 2 );
+  }
+
+  protected handleBoundaryBehavior( dt: number, particle: Particle<IntentionalAny> ): void {
+    if ( this.direction === 'inward' && ( particle.position.y + particle.dimension.height / 2 ) < MembraneTransportConstants.MEMBRANE_BOUNDS.minY ) {
+      const downwardDirection = new Vector2(
+        dotRandom.nextDoubleBetween( -1, 1 ),
+        dotRandom.nextDoubleBetween( -1, 0 )
+      ).normalize();
+      particle.moveInDirection( downwardDirection, this.sampleValueHowLongToGoStraight() );
+    }
+    if ( this.direction === 'outward' && ( particle.position.y - particle.dimension.height / 2 ) > MembraneTransportConstants.MEMBRANE_BOUNDS.maxY ) {
+      const upwardDirection = new Vector2(
+        dotRandom.nextDoubleBetween( -1, 1 ),
+        dotRandom.nextDoubleBetween( 0, 1 )
+      ).normalize();
+      particle.moveInDirection( upwardDirection, this.sampleValueHowLongToGoStraight() );
+    }
+  }
+
+  protected abstract handleSpecificBehavior( dt: number, particle: Particle<IntentionalAny>, model: MembraneTransportModel ): void;
+}
+
+membraneTransport.register( 'DirectionalMovementMode', DirectionalMovementMode );
--- js/common/model/particleModes/MovingThroughTransportProteinMode.ts
+++ js/common/model/particleModes/MovingThroughTransportProteinMode.ts
@@ -7,10 +7,6 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import dotRandom from '../../../../../dot/js/dotRandom.js';
-import { boxMullerTransform } from '../../../../../dot/js/util/boxMullerTransform.js';
-import { clamp } from '../../../../../dot/js/util/clamp.js';
-import Vector2 from '../../../../../dot/js/Vector2.js';
 import IntentionalAny from '../../../../../phet-core/js/types/IntentionalAny.js';
 import membraneTransport from '../../../membraneTransport.js';
 import MembraneTransportConstants from '../../MembraneTransportConstants.js';
@@ -19,15 +15,15 @@ import Particle from '../Particle.js';
 import SodiumGlucoseCotransporter from '../proteins/SodiumGlucoseCotransporter.js';
 import TransportProteinType from '../proteins/TransportProteinType.js';
 import Slot from '../Slot.js';
-import BaseParticleMode from './BaseParticleMode.js';
+import DirectionalMovementMode from './DirectionalMovementMode.js';
 
-export default class MovingThroughTransportProteinMode extends BaseParticleMode {
+export default class MovingThroughTransportProteinMode extends DirectionalMovementMode {
 
   public constructor( public readonly slot: Slot,
                       public readonly transportProteinType: TransportProteinType,
-                      public readonly direction: 'inward' | 'outward',
+                      direction: 'inward' | 'outward',
                       public readonly offset?: number ) {
-    super( 'movingThroughTransportProtein' );
+    super( 'movingThroughTransportProtein', direction );
   }
 
   public override toStateObject(): IntentionalAny {
@@ -40,20 +36,7 @@ export default class MovingThroughTransportProteinMode extends BaseParticleMode 
     };
   }
 
-  public step( dt: number, particle: Particle<IntentionalAny>, model: MembraneTransportModel ): void {
-    const sign = this.direction === 'inward' ? -1 : 1;
-    const signBefore = particle.position.y > 0;
-    const TYPICAL_SPEED = 30;
-
-    particle.position.y += sign * ( TYPICAL_SPEED / 5 ) * dt * dotRandom.nextDoubleBetween( 0.1, 2 );
-    particle.position.x += dotRandom.nextDoubleBetween( -2, 2 ) * ( TYPICAL_SPEED / 2 ) * dt;
-
-    const signAfter = particle.position.y > 0;
-
-    if ( signBefore !== signAfter ) {
-      model.soluteCrossedMembraneEmitter.emit( particle, particle.position.y > 0 ? 'outward' : 'inward' );
-    }
-
+  protected handleSpecificBehavior( dt: number, particle: Particle<IntentionalAny>, model: MembraneTransportModel ): void {
     const center = this.slot.position + ( this.offset || 0 );
     const maxDistanceFromCenter = 0.8;
     if ( Math.abs( particle.position.x - center ) > maxDistanceFromCenter ) {
@@ -69,26 +52,6 @@ export default class MovingThroughTransportProteinMode extends BaseParticleMode 
       if ( transportProtein instanceof SodiumGlucoseCotransporter ) {
         transportProtein.stateProperty.value = 'openToInside';
       }
-    }
-
-    const sampleValueHowLongToGoStraight = () => {
-      const result = boxMullerTransform( 0.3, 0.4, dotRandom );
-      return clamp( result, 0.01, 2 );
-    };
-
-    if ( this.direction === 'inward' && ( particle.position.y + particle.dimension.height / 2 ) < MembraneTransportConstants.MEMBRANE_BOUNDS.minY ) {
-      const downwardDirection = new Vector2(
-        dotRandom.nextDoubleBetween( -1, 1 ),
-        dotRandom.nextDoubleBetween( -1, 0 )
-      ).normalize();
-      particle.moveInDirection( downwardDirection, sampleValueHowLongToGoStraight() );
-    }
-    if ( this.direction === 'outward' && ( particle.position.y - particle.dimension.height / 2 ) > MembraneTransportConstants.MEMBRANE_BOUNDS.maxY ) {
-      const upwardDirection = new Vector2(
-        dotRandom.nextDoubleBetween( -1, 1 ),
-        dotRandom.nextDoubleBetween( 0, 1 )
-      ).normalize();
-      particle.moveInDirection( upwardDirection, sampleValueHowLongToGoStraight() );
     }
   }
 
