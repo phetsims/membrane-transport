13659f7aad40f37293d25ecd69b8542c8c3ca060
Jesse <jesse.greenberg@colorado.edu>
2025-07-09 16:38:27 -0400

    combine animateProteinReturn and createPositionAnimation, then delete createPositionAnimation, see https://github.com/phetsims/membrane-transport/issues/313

--- js/common/view/animateProteinReturn.ts
+++ js/common/view/animateProteinReturn.ts
@@ -2,16 +2,17 @@
 
 /**
  * Utility for animating a transport protein node moving back to the toolbox, often
- * when a slot is replaced or canceled.
+ * when a slot is replaced or canceled. Disposes the node upon completion.
  *
  * @author Jesse Greenberg (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
  */
 
 import Vector2 from '../../../../dot/js/Vector2.js';
-import membraneTransport from '../../membraneTransport.js';
-import createPositionAnimation from './createPositionAnimation.js';
-import TransportProteinDragNode from './TransportProteinDragNode.js';
 import Animation from '../../../../twixt/js/Animation.js';
+import Easing from '../../../../twixt/js/Easing.js';
+import membraneTransport from '../../membraneTransport.js';
+import TransportProteinDragNode from './TransportProteinDragNode.js';
 
 /**
  * Animates a protein node returning from its current model position
@@ -27,15 +28,21 @@ export default function animateProteinReturn(
   toPosition: Vector2,
   onComplete?: () => void
 ): Animation {
-  const animation = createPositionAnimation(
-    position => proteinNode.setModelPosition( position ),
-    proteinNode.getModelPosition(),
-    toPosition,
-    () => {
-      proteinNode.dispose();
-      onComplete && onComplete();
-    }
-  );
+  const animation = new Animation( {
+    setValue: function( value: Vector2 ) {
+      proteinNode.setModelPosition( value );
+    },
+    from: proteinNode.getModelPosition().copy(),
+    to: toPosition,
+    duration: 0.4,
+    easing: Easing.CUBIC_IN_OUT
+  } );
+
+  animation.endedEmitter.addListener( () => {
+    proteinNode.dispose();
+    onComplete && onComplete();
+  } );
+
   animation.start();
 
   return animation;
