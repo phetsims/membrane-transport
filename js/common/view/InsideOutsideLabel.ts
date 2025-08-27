033bf5ac76cdf3eabf46925703628a2a1db5285c
Sam Reid <reids@colorado.edu>
2025-08-26 21:49:13 -0600

    Hide text background, see https://github.com/phetsims/membrane-transport/issues/370

--- js/common/view/InsideOutsideLabel.ts
+++ js/common/view/InsideOutsideLabel.ts
@@ -8,20 +8,19 @@
 
 import { LayoutNodeOptions } from '../../../../scenery/js/layout/nodes/LayoutNode.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
-import Color from '../../../../scenery/js/util/Color.js';
 import Panel from '../../../../sun/js/Panel.js';
 import membraneTransport from '../../membraneTransport.js';
 import MembraneTransportFluent from '../../MembraneTransportFluent.js';
-import MembraneTransportConstants from '../MembraneTransportConstants.js';
-
-const panelOptions = { cornerRadius: MembraneTransportConstants.PANEL_CORNER_RADIUS, stroke: null, fill: Color.WHITE.withAlpha( 0.4 ) };
 
 export default class InsideOutsideLabel extends Panel {
   public constructor( side: 'inside' | 'outside', textMaxWidth: number, options?: LayoutNodeOptions ) {
     super( new Text( side === 'outside' ? MembraneTransportFluent.cellRegions.outsideStringProperty : MembraneTransportFluent.cellRegions.insideStringProperty, {
-      fontSize: 13,
-      maxWidth: textMaxWidth
-    } ), panelOptions );
+        fontSize: 13,
+        maxWidth: textMaxWidth
+      } ),
+
+      // invisible panel just provides margins
+      { stroke: null, fill: null } );
 
     options && this.mutate( options );
   }
