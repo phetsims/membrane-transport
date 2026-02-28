b39d5b79d1705ee6ff6be08a783467ff3a33f41b
phet-dev <phetadmin@gmail.com>
2026-02-28 04:49:28 -0700

    update copyright dates from daily grunt work

--- js/common/view/MembraneDescriber.ts
+++ js/common/view/MembraneDescriber.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * Implements descriptions relating to the state of the membrane. Includes information about
