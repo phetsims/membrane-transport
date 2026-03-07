dae39da592b2686c58ce9b1ac9b843da7e55fce5
phet-dev <phetadmin@gmail.com>
2026-03-07 06:38:14 -0700

    update copyright dates from daily grunt work

--- js/common/view/SoluteBarChartNode.ts
+++ js/common/view/SoluteBarChartNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * In the SoluteConcentrationsAccordionBox, the node that shows an icon and bar chart representing the
